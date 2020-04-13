126b4b4d45c86c65efb0e77e140c4066013aab73
ChiefBitCounter <rconley6836@comcast.net>
2020-04-13 18:26:22 -0400

    basic JS

---
+++ JS/Basic_Javascript_1.js
@@ -0,0 +1,23 @@
+var A = "This is just some text to say \"WHAAASAAHHHHHHH\"";
+var B = "This is string one " + " This is string two too!"
+
+var kid = "devlin", dad= "dom", mom="sherry"
+
+var B = B.fontcolor("red");
+
+var C = (9+10);
+
+window.alert(A);
+
+document.write(B);
+document.writeln();
+document.write(mom);
+document.writeln();
+document.write(C);
+
+
+function My_First_Function() {
+    var str= "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
