a45c3a7aaa7c532e59025abc2ba31c3829a651d4
ChiefBitCounter <rconley6836@comcast.net>
2020-04-14 09:40:11 -0400

    html JS update

---
+++ JS/Basic_Javascript_2.js
@@ -0,0 +1,12 @@
+function assignTwoVar() {
+    var A = 5;
+    var B = 6;
+    result = A+B;
+    document.getElementById("one").innerHTML = A+B+"!";
+}
+
+function conCatFunction() {
+    conCat = "There are three kinds of people in this world. ";
+    conCat += "Those who can count, and those who can't.";
+    document.getElementById("two").innerHTML = conCat;
+}
